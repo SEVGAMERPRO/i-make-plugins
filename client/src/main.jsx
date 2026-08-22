0cce62c37000fe43a55984e7017ab3e57d150c1e
SEVGAMERPRO <sevgamerpro@users.noreply.github.com>
2026-08-22 21:29:36 +0200

    Update Google OAuth with official project client ID and secret

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
 import './index.css'
 
-const GOOGLE_CLIENT_ID = "444694272527-cuu2sa522v4omoo6n4r88fab9dutaour.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "236275142196-ja1sa5fcr748vlojug2pbg33d1jk5hor.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
